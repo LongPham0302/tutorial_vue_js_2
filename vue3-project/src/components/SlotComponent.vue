bf6fa5fb99a894d33061c2ecb86c3b959d32420d
LongPham0302 <bekeo0302@gmail.com>
2024-05-06 10:53:17 +0700

    demo slot

---
+++ vue3-project/src/components/SlotComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <slot></slot>
+      <!-- mặc định slot là nội dùng bạn để giữa 2 thẻ mở và đóng , ở đây là <SlotComponent> và <SlotComponent/> . -->
+      <div v-if="$slots.details">
+        <slot name="details"></slot>
+        <!-- khi bạn # tên slot thì sẽ tương ứng vị trí của slot đó  , ở SlotView tôi đặt # details thì nội dung sẽ tương ứng với vị trị slot có name là details -->
+      </div>
+      
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
---
+++ vue3-project/src/views/SlotView.vue
@@ -0,0 +1,17 @@
+<!-- Template -->
+<template>
+  <div>
+    <!-- Sử dụng SlotComponent với khe (slot) có tên "details" -->
+    <SlotComponent>
+      Đây là nội dung mặc định.
+      <template #details>
+        Đây là nội dung chi tiết.
+      </template>
+    </SlotComponent>
+  </div>
+</template>
+
+<!-- Script -->
+<script setup>  
+import SlotComponent from '@/components/SlotComponent.vue';
+</script>
